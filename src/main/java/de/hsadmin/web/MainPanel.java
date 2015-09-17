1961ea205fc1e02530f61eb06da49445e3fa7293
Peter Hormanns <peter.hormanns@jalin.de>
2015-09-17 15:27:18 +0200

    i18n example code

--- src/main/java/de/hsadmin/web/MainPanel.java
+++ src/main/java/de/hsadmin/web/MainPanel.java
@@ -1,5 +1,7 @@
 package de.hsadmin.web;
 
+import java.util.ResourceBundle;
+
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
@@ -9,9 +11,12 @@ public class MainPanel extends CustomComponent{
 	private static final long serialVersionUID = -1085100738394404620L;
 
 	public MainPanel() {
+		final ResourceBundle bundle = ResourceBundle.getBundle("de.hsadmin.web.main");
 		final Label dialog = new Label();
-		final Panel panel  = new Panel("Main Panel");
-		dialog.setValue("Welcome to the hostsharing admin page");
+		final String title = bundle.getString("main.panel.title");
+		final Panel panel  = new Panel(title);
+		final String text = bundle.getString("main.panel.text");
+		dialog.setValue(text);
 		panel.setContent(dialog);
 		setCompositionRoot(panel);
 	}
